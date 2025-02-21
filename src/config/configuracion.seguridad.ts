9826752749e9e95056f6a89b4f468a74550f9b00
SrJaniot <estebanjaniot10@gmail.com>
2025-02-21 15:35:02 -0500

    first commit

---
+++ src/config/configuracion.seguridad.ts
@@ -0,0 +1,23 @@
+
+export namespace ConfiguracionSeguridad{
+  //-------------------------VARIABLES DE ENTORNO  -------------------------------------
+    //instalar el paquete dotenv npm i dotenv para poder leer variables de entorno  y importar en application.ts require('dotenv').config();
+    export const connection_user_postgres = process.env.CONNECTION_USER_POSTGRES ;
+    export const connection_password_postgres = process.env.CONNECTION_PASSWORD_POSTGRES ;
+    export const connection_database_postgres = process.env.CONNECTION_DATABASE_POSTGRES ;
+    export const connection_host_postgres = process.env.CONNECTION_HOST_POSTGRES ;
+    export const connection_port_postgres = process.env.CONNECTION_PORT_POSTGRES ;
+    
+    //
+    //cambiar esta ruta por la url del server con el puerto a la api de seguridad
+    export const hostSeguridad = process.env.CONECTION_SEGURIDAD ;
+
+
+  //-------------------------menus -------------------------------------
+ 
+  //-------------------------acciones -------------------------------------
+
+
+
+
+}
---
+++ src/datasources/postgres.datasource.ts
@@ -0,0 +1,32 @@
+import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
+import {juggler} from '@loopback/repository';
+import { ConfiguracionSeguridad } from '../config/configuracion.seguridad';
+
+const config = {
+  name: 'postgres',
+  connector: 'postgresql',
+  url: '',
+  host: ConfiguracionSeguridad.connection_host_postgres,
+  port: ConfiguracionSeguridad.connection_port_postgres,
+  user: ConfiguracionSeguridad.connection_user_postgres,
+  password: ConfiguracionSeguridad.connection_password_postgres,
+  database: ConfiguracionSeguridad.connection_database_postgres
+};
+
+// Observe application's life cycle to disconnect the datasource when
+// application is stopped. This allows the application to be shut down
+// gracefully. The `stop()` method is inherited from `juggler.DataSource`.
+// Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
+@lifeCycleObserver('datasource')
+export class PostgresDataSource extends juggler.DataSource
+  implements LifeCycleObserver {
+  static dataSourceName = 'postgres';
+  static readonly defaultConfig = config;
+
+  constructor(
+    @inject('datasources.config.postgres', {optional: true})
+    dsConfig: object = config,
+  ) {
+    super(dsConfig);
+  }
+}
